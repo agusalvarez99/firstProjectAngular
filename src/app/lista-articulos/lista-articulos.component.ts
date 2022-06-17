4e81b82ebcc26f437fbca358b248533ba6fcee4f
agusalvarez99 <agusalvarez99@hotmail.com>
2022-06-17 15:00:56 -0300

    directivas

--- src/app/lista-articulos/lista-articulos.component.ts
+++ src/app/lista-articulos/lista-articulos.component.ts
@@ -7,6 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaArticulosComponent implements OnInit {
   titulo: string = 'List of articles';
+  articulos: any[] = [
+    {
+      id: 2,
+      descripcion: 'Article X',
+      codigo: 'xsd-143',
+      cantidad: 139,
+      precio: 1221.4,
+      puntaje: 4,
+    },
+    {
+      id: 5,
+      descripcion: 'Article Y',
+      codigo: 'dlg-912',
+      cantidad: 336,
+      precio: 400.99,
+      puntaje: 3,
+    },
+  ];
   constructor() {}
 
   ngOnInit() {}

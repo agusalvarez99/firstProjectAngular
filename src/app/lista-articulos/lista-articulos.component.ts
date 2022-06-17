172a15289013029d79353e6924e1a6d3398fa8fe
agusalvarez99 <agusalvarez99@hotmail.com>
2022-06-17 15:15:05 -0300

    property binding

--- src/app/lista-articulos/lista-articulos.component.ts
+++ src/app/lista-articulos/lista-articulos.component.ts
@@ -7,8 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaArticulosComponent implements OnInit {
   titulo: string = 'List of articles';
+  alturaImagen: number = 40;
+  margenImagen: number = 2;
   articulos: any[] = [
     {
+      imagen:
+        'https://media.istockphoto.com/vectors/dirty-grunge-hand-drawn-with-brush-strokes-cross-x-vector-isolated-vector-id1201202836?k=20&m=1201202836&s=612x612&w=0&h=0ib7KQaJXonfKN0-tGNaMUIw2Hre9sJjd4hTsc3QwBc=',
       id: 2,
       descripcion: 'Article X',
       codigo: 'xsd-143',
@@ -17,6 +21,8 @@ export class ListaArticulosComponent implements OnInit {
       puntaje: 4,
     },
     {
+      imagen:
+        'https://png.pngtree.com/png-vector/20210313/ourlarge/pngtree-letter-y-logo-png-png-image_3045053.jpg',
       id: 5,
       descripcion: 'Article Y',
       codigo: 'dlg-912',
